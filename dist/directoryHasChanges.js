348ad3a8acd7539d29a94740bcdec36865380e9a
yanats4rb <yan.shcherbakov@s4rb.com>
2020-10-05 14:14:04 +0300

    Build and package.

--- dist/directoryHasChanges.js
+++ dist/directoryHasChanges.js
@@ -1,9 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@actions/core");
-const fileMatchesFolder = (filename, directory) => !!filename.match(`(?<!/)${directory}/.*`);
+const minimatch_1 = __importDefault(require("minimatch"));
 exports.default = (files, directory) => {
-    const changedFiles = files.filter(filename => fileMatchesFolder(filename, directory));
+    const changedFiles = files.filter(filename => minimatch_1.default(filename, directory));
     core_1.info(`Changed Files: ${changedFiles.join(' ')}`);
     return changedFiles.length > 0;
 };
